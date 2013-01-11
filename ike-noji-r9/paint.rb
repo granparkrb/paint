b2ff7f4796b49acf413f920e3104db9010dd4488
rightgo09 <skyarrow09@gmail.com>
2013-01-11 20:18:54 +0900

    あと少しだった・・・

--- ike-noji-r9/paint.rb
+++ ike-noji-r9/paint.rb
@@ -19,34 +19,45 @@ file_content.split(/\n/).each_with_index do |line, i|
 end
 
 door_size = door.length
-brush_size = 2
-door.each_with_index do |col, i|
-  col.each_with_index do |cell, j|
-    paintable = true
-    catch(:paintable) do
-      (0...brush_size).each do |k|
-        (0...brush_size).each do |l|
-          if door[i+k].nil? || door[i+k][j+l].nil?
-            next
+(2..door_size).each do |brush_size|
+  door.each_with_index do |col, i|
+    col.each_with_index do |cell, j|
+      paintable = true
+      catch(:paintable) do
+        (0...brush_size).each do |k|
+          (0...brush_size).each do |l|
+            if door[i+k].nil? || door[i+k][j+l].nil?
+              next
+            end
+            if door[i+k][j+l].is_window
+              paintable = false
+              throw :paintable
+            end
           end
-          if door[i+k][j+l].is_window
-            paintable = false
-            throw :paintable
+        end
+      end
+
+      if paintable
+        (0...brush_size).each do |k|
+          (0...brush_size).each do |l|
+            if door[i+k].nil? || door[i+k][j+l].nil?
+              next
+            end
+            door[i+k][j+l].painted = true
           end
         end
       end
     end
+  end
 
-    if paintable
-      (0...brush_size).each do |k|
-        (0...brush_size).each do |l|
-          if door[i+k].nil? || door[i+k][j+l].nil?
-            next
-          end
-          door[i+k][j+l].painted = true
-        end
+  door.each do |col|
+    col.each do |cell|
+      if !cell.is_window && !cell.painted
+        puts brush_size - 1
+        exit
       end
     end
   end
 end
 
+puts door_size
